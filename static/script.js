dc451423b09741f1a17438a17a3edc855d695b50
shouvik <shouvikm30@gmail.com>
2023-09-02 21:44:46 +0400

    gg

--- static/script.js
+++ static/script.js
@@ -1,18 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const members = document.querySelectorAll(".member");
-  let delay = 0;
-
-  members.forEach((member, index) => {
-    setTimeout(() => {
-      member.style.transitionDelay = `${index * 0.3}s`;
-      member.style.opacity = 1;
-      member.style.transform = "translateY(0)";
-    }, delay * 1000);
-    
-    delay += 0.2;
-  });
-});
-
 let menu1 = document.querySelector("#menu-icon");
 let navlist1 = document.querySelector(".navlist");
 let header1 = document.querySelector("header");
